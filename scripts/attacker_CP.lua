cb823f5f33749154cfba483c5fb520f26ac9b9bd
klortiz13 <kortizalvarado@ucsd.edu>
2025-12-28 11:08:03 -0800

    Added global variables and reset functionality to the attacket cp counter

--- scripts/attacker_CP.lua
+++ scripts/attacker_CP.lua
@@ -1,23 +1,22 @@
 -- Change path to where you have the round_count.txt file stored --
 Obslua = Obslua or require("obslua")
+local Global = require("global_variables")
 local file_path = "C:/Users/leona/OneDrive/Documents/Code/Warhammer_OBS_Sources/data/attacker_CP.txt"
-local hotkey_id = Obslua.OBS_INVALID_HOTKEY_ID
+local hotkey_increment_id = Obslua.OBS_INVALID_HOTKEY_ID
+local hotkey_reset_id = Obslua.OBS_INVALID_HOTKEY_ID
 
 -- Reads the text file round_count.txt and increments its value by 1 --
-local function increment_round()
+local function increment_attacker_cp()
     local file = io.open(file_path, "r")
     if not file then
         print("Could not open file: " .. file_path)
         return
     end
-
+    -- get contents
     local contents = file:read("*all")
     contents = contents:gsub("%s+", "")
     local number = tonumber(contents) or 0
-
     file:close()
-
-    number = number + 1
 
     file = io.open(file_path, "w")
     if not file then
@@ -25,34 +24,70 @@ local function increment_round()
         return
     end
 
-    file:write(tostring(number))
+    -- update global and write count
+    Global.AttackerCP = number + 1
+    file:write(tostring(Global["AttackerCP"]))
+    
     file:close()
 end
 
-local function on_hotkey(pressed)
+local function on_increment_hotkey(pressed)
     if not pressed then return end
-    increment_round()
+    increment_attacker_cp()
+end
+
+local function reset_attacker_cp()
+    local file = io.open(file_path, "w")
+    if not file then
+        print("Could not write file")
+        return
+    end
+
+    -- update global and write count
+    Global.AttackerCP = 0
+    file:write(tostring(0))
+    file:close()
+end
+
+local function on_reset_hotkey(pressed)
+    if not pressed then return end
+    reset_attacker_cp()
 end
 
 -- This is the name and description that will be displayed in OBS -> Settings -> Hotkeys --
 function script_load(settings)
-    hotkey_id = Obslua.obs_hotkey_register_frontend(
+    hotkey_increment_id = Obslua.obs_hotkey_register_frontend(
         "increment_attacker_cp_hotkey",
         "[From Lua Script] Attacker CP",
-        on_hotkey
+        on_increment_hotkey
     )
 
-    local hotkey_array = Obslua.obs_data_get_array(settings, "increment_attacker_cp_hotkey")
-    Obslua.obs_hotkey_load(hotkey_id, hotkey_array)
-    Obslua.obs_data_array_release(hotkey_array)
+    local increment_attacker_cp_hotkey_array = Obslua.obs_data_get_array(settings, "increment_attacker_cp_hotkey")
+    Obslua.obs_hotkey_load(hotkey_increment_id, increment_attacker_cp_hotkey_array)
+    Obslua.obs_data_array_release(increment_attacker_cp_hotkey_array)
+    
+    hotkey_reset_id = Obslua.obs_hotkey_register_frontend(
+        "reset_attacker_cp_hotkey",
+        "[From Lua Script] Reset Attacker CP",
+        on_reset_hotkey
+    )
+
+    local reset_attacker_cp_hotkey_array = Obslua.obs_data_get_array(settings, "reset_attacker_cp_hotkey")
+    Obslua.obs_hotkey_load(hotkey_reset_id, reset_attacker_cp_hotkey_array)
+    Obslua.obs_data_array_release(reset_attacker_cp_hotkey_array)
+
 end
 
 function script_description()
-    return "Attacker CP – increments the Attacker's CP in data/attacker_CP.txt"
+    return "Attacker CP – increments and resets the Attacker's CP located in data/attacker_CP.txt"
 end
 
 function script_save(settings)
-    local hotkey_array = Obslua.obs_hotkey_save(hotkey_id)
-    Obslua.obs_data_set_array(settings, "increment_attacker_cp_hotkey", hotkey_array)
-    Obslua.obs_data_array_release(hotkey_array)
+    local increment_attacker_cp_hotkey_array = Obslua.obs_hotkey_save(hotkey_increment_id)
+    Obslua.obs_data_set_array(settings, "increment_attacker_cp_hotkey", increment_attacker_cp_hotkey_array)
+    Obslua.obs_data_array_release(increment_attacker_cp_hotkey_array)
+
+    local reset_attacker_cp_hotkey_array = Obslua.obs_hotkey_save(hotkey_reset_id)
+    Obslua.obs_data_set_array(settings, "reset_attacker_cp_hotkey", reset_attacker_cp_hotkey_array)
+    Obslua.obs_data_array_release(reset_attacker_cp_hotkey_array)
 end
---
+++ scripts/global_variables.lua
@@ -0,0 +1,48 @@
+local M = {}
+-- Global variables for the primary mission scripts
+M.Primaries = {
+    "TAKE AND HOLD",
+    "BURDEN OF TRUST",
+    "LINCHPIN",
+    "SCORCHED EARTH",
+    "PURGE THE FOE",
+    "TERRAFORM",
+    "HIDDEN SUPPLIES",
+    "THE RITUAL",
+    "UNEXPLODED ORDINANCE",
+    "SUPPLY DROP"
+}
+M.PrimarySize = #M.Primaries
+M.PrimaryIndex = 1
+
+-- Global variables for the secondary mission scripts
+M.Secondaries = {
+    "SABOTAGE",
+    "STORM HOSTILE OBJECTIVE",
+    "ESTABLISH LOCUS",
+    "CLEANSE",
+    "A TEMPTING TARGET",
+    "RECOVER ASSETS",
+    "ENGAGE ON ALL FRONTS",
+    "ASSASSINATION",
+    "BRING IT DOWN",
+    "CULL THE HORDE",
+    "NO PRISONERS",
+    "SECURE NO MAN\'S LAND",
+    "MARKED FOR DEATH",
+    "DEFEND STRONGHOLD",
+    "AREA DENIAL",
+    "OVERWHELMING FORCE",
+    "BEHIND ENEMY LINES",
+    "DISPLAY OF MIGHT",
+    "EXTEND BATTLE LINES"
+}
+M.SecondarySize = #M.Secondaries
+M.SecondaryIndex = 1
+
+-- Global variables for the counter files
+M.AttackerCP = 0
+M.DefenderCP = 0
+M.BattleRound = 0
+
+return M
