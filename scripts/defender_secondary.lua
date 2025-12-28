ed2e2ce655e3ab123545e5228f873f865bafcc88
klortiz13 <kortizalvarado@ucsd.edu>
2025-12-28 09:42:03 -0800

    Finished all scripts

---
+++ scripts/defender_secondary.lua
@@ -0,0 +1,68 @@
+-- Change path to where you have the round_count.txt file stored --
+Obslua = Obslua or require("obslua")
+local file_path = "C:/Users/leona/OneDrive/Documents/Code/Warhammer_OBS_Sources/data/defender_secondary.txt"
+local hotkey_id = Obslua.OBS_INVALID_HOTKEY_ID
+local secondaries = {
+    "SABOTAGE",
+    "STORM HOSTILE OBJECTIVE",
+    "ESTABLISH LOCUS",
+    "CLEANSE",
+    "A TEMPTING TARGET",
+    "RECOVER ASSETS",
+    "ENGAGE ON ALL FRONTS",
+    "ASSASSINATION",
+    "BRING IT DOWN",
+    "CULL THE HORDE",
+    "NO PRISONERS",
+    "SECURE NO MAN\'S LAND",
+    "MARKED FOR DEATH",
+    "DEFEND STRONGHOLD",
+    "AREA DENIAL",
+    "OVERWHELMING FORCE",
+    "BEHIND ENEMY LINES",
+    "DISPLAY OF MIGHT",
+    "EXTEND BATTLE LINES"
+}
+local size = #secondaries
+local index = 1
+
+-- Reads the text file round_count.txt and increments its value by 1 --
+local function increment_round()
+    local file = io.open(file_path, "w")
+    if not file then
+        print("Could not write file")
+        return
+    end
+
+    index = (index < size) and (index + 1) or 1
+    file:write(secondaries[index])
+    file:close()
+end
+
+local function on_hotkey(pressed)
+    if not pressed then return end
+    increment_round()
+end
+
+-- This is the name and description that will be displayed in OBS -> Settings -> Hotkeys --
+function script_load(settings)
+    hotkey_id = Obslua.obs_hotkey_register_frontend(
+        "change_defender_secondary_hotkey",
+        "[From Lua Script] Defender Secondary",
+        on_hotkey
+    )
+
+    local hotkey_array = Obslua.obs_data_get_array(settings, "change_defender_secondary_hotkey")
+    Obslua.obs_hotkey_load(hotkey_id, hotkey_array)
+    Obslua.obs_data_array_release(hotkey_array)
+end
+
+function script_description()
+    return "Defender Secondary – Changes the Defender's Secondary in data/defender_secondary.txt"
+end
+
+function script_save(settings)
+    local hotkey_array = Obslua.obs_hotkey_save(hotkey_id)
+    Obslua.obs_data_set_array(settings, "change_defender_secondary_hotkey", hotkey_array)
+    Obslua.obs_data_array_release(hotkey_array)
+end
---
+++ scripts/primary_mission.lua
@@ -0,0 +1,59 @@
+-- Change path to where you have the round_count.txt file stored --
+Obslua = Obslua or require("obslua")
+local file_path = "C:/Users/leona/OneDrive/Documents/Code/Warhammer_OBS_Sources/data/primary_mission.txt"
+local hotkey_id = Obslua.OBS_INVALID_HOTKEY_ID
+local primaries = {
+    "TAKE AND HOLD",
+    "BURDEN OF TRUST",
+    "LINCHPIN",
+    "SCORCHED EARTH",
+    "PURGE THE FOE",
+    "TERRAFORM",
+    "HIDDEN SUPPLIES",
+    "THE RITUAL",
+    "UNEXPLODED ORDINANCE",
+    "SUPPLY DROP"
+}
+local size = #primaries
+local index = 1
+
+-- Reads the text file round_count.txt and increments its value by 1 --
+local function increment_round()
+    local file = io.open(file_path, "w")
+    if not file then
+        print("Could not write file")
+        return
+    end
+
+    index = (index < size) and (index + 1) or 1
+    file:write(primaries[index])
+    file:close()
+end
+
+local function on_hotkey(pressed)
+    if not pressed then return end
+    increment_round()
+end
+
+-- This is the name and description that will be displayed in OBS -> Settings -> Hotkeys --
+function script_load(settings)
+    hotkey_id = Obslua.obs_hotkey_register_frontend(
+        "change_primary_mission_hotkey",
+        "[From Lua Script] Primary Mission",
+        on_hotkey
+    )
+
+    local hotkey_array = Obslua.obs_data_get_array(settings, "change_primary_mission_hotkey")
+    Obslua.obs_hotkey_load(hotkey_id, hotkey_array)
+    Obslua.obs_data_array_release(hotkey_array)
+end
+
+function script_description()
+    return "Primary Mission – Changes the Primary Mission in data/primary_mission.txt"
+end
+
+function script_save(settings)
+    local hotkey_array = Obslua.obs_hotkey_save(hotkey_id)
+    Obslua.obs_data_set_array(settings, "change_primary_mission_hotkey", hotkey_array)
+    Obslua.obs_data_array_release(hotkey_array)
+end
